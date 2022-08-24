8a6ca09ac4321950793f31de71103fee05c7a5f4
Mehdi Anhichem <m.anhichem@liverpool.ac.uk>
2022-08-24 18:07:11 +0100

    Update 24082022

--- mehdi_work/preprocessing/cpmapping_2_3Dmat.py
+++ mehdi_work/preprocessing/cpmapping_2_3Dmat.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-File to convert pressure coefficient data (chordwise evolution at one serial at one Y) to a 1D .mat file.
+File to convert pressure coefficient data (chordwise evolution at one serial at one Y
+with experimental conditions variations) to a 3D .mat file.
 
 File used for pre-processing on the GPSS research framework.
 
 Mehdi Anhichem
 University of Liverpool
-30/07/2022
+24/08/2022
 """
 
 #==============================================================================
@@ -51,27 +52,13 @@ pressure_taps_data = IS_pressure_taps.df
 print('### Data generation finished ###')
 
 os.chdir(test_path)
-print('### Filtering data at A =  3.01 ; M = 0.82 ###')
-df_pressure_taps_serial = IS_pressure_taps.filter_serial(310)
 
-# Remove one outlier
-df_pressure_taps_serial = df_pressure_taps_serial.loc[(df_pressure_taps_serial['Cp']<1.0)]
+# Remove Mach = 0
+pressure_taps_data = pressure_taps_data.loc[(pressure_taps_data['Mach']>0.1)]
 
 # Data selection
-pressure_taps_data_yfix = df_pressure_taps_serial.loc[(df_pressure_taps_serial['y']==574.4)]
+pressure_taps_data_yfix = pressure_taps_data.loc[(pressure_taps_data['y']==574.4)]
 X_t_yfix = pressure_taps_data_yfix['x'].values.reshape(-1,1)
 Y_t_yfix = pressure_taps_data_yfix['Cp'].values.reshape(-1,1)
 print(X_t_yfix.shape, Y_t_yfix.shape)
-
-# Plot data
-plt.figure(figsize=(8, 6))
-plt.scatter(X_t_yfix, -Y_t_yfix, s = 25, marker = 'x', c = 'k')
-plt.xlabel('x')
-plt.ylabel('-Cp')
-plt.plot()
-
-# # Save as a .mat file
-# from scipy.io import savemat
-# mdic = {"X": X_t_yfix, "y": Y_t_yfix, "Xtest": X_t_yfix, "ytest": Y_t_yfix}
-# savemat("../data/test_1d.mat", mdic)
 
